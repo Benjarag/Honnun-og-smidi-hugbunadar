463d139f69d78ea9ccaa7b159b425e08ff6604dd
Benjamín Ragnarsson <benjarag7@outlook.com>
2024-10-29 17:03:58 +0000

    eftir tima

--- VIKA11/lab_10/MessageProcessor/main.py
+++ VIKA11/lab_10/MessageProcessor/main.py
@@ -4,10 +4,38 @@ from retry import retry
 
 @retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
 def get_connection():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            "localhost", credentials=pika.PlainCredentials("guest", "guest")
+        )
+    )
+    return connection
     # TODO: create rabbitmq connection
-    pass
-
+    
+def callback(ch, method, properties, body):
+    message = body.decode()
+    print(f"Received message: {message}")  # Print the received message
+    
+    # Write the message to a text file
+    with open('messages.txt', 'a') as f:
+        f.write(message + '\n')  # Append the message to the file
 
 if __name__ == '__main__':
     # TODO: consume message events and print them to console
-    pass
+    connection = get_connection()
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+    channel.queue_declare(queue='queue')
+
+    channel.queue_bind(exchange='logs', queue='queue')
+
+    print('Waiting for logs....')
+    # TODO: consume logs here
+    channel.basic_consume(
+        queue="queue", auto_ack=True, on_message_callback=callback
+    )
+    channel.start_consuming()
+
+
